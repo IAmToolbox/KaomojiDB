13e65cfbaa4ce9935fdb87cde34a906337ec3917
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-05-10 19:42:13 -0500

    Implemented test scripts to manipulate the json database

--- scripts/data_manip.py
+++ scripts/data_manip.py
@@ -2,3 +2,19 @@
 
 import json
 
+def print_json():
+    with open("data/database.json", encoding="utf-8") as j:
+        raw_json = json.load(j)
+    print(raw_json)
+
+def write_json():
+    test_dictionary = {"smiley": ":)"}
+    json_object = json.dumps(test_dictionary, indent=4)
+    with open("data/database.json", "w", encoding="utf-8") as j:
+        j.write(json_object)
+
+def overwrite_json():
+    empty_dictionary = {}
+    json_object = json.dumps(empty_dictionary)
+    with open("data/database.json", "w", encoding="utf-8") as j:
+        j.write(json_object)
--- scripts/main.py
+++ scripts/main.py
@@ -3,7 +3,13 @@
 
 # This is the main file, where everything comes together
 
+from data_manip import *
+
 def main():
     print("Welcome to KaomojiDB. This is just a placeholder message for now.")
+    write_json()
+    print_json()
+    overwrite_json()
+    print_json()
 
 main()
