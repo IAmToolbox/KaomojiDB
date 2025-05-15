822948fc398a4d3085e4f3bca2f451b668a23e1a
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-05-14 19:57:42 -0500

    Added the ability to add items to the database, as well as reset it to zero. Added a basic main menu as well

--- scripts/data_manip.py
+++ scripts/data_manip.py
@@ -8,16 +8,22 @@ database = Database(None)
 def load_database():
     with open("data/database.json", encoding="utf-8") as j:
         raw_json = json.load(j)
-    database.database = raw_json
+        database.database = raw_json
     print("Database successfully loaded!")
     
+def add_item():
+    new_kaomoji = input("Please paste your new kaomoji:")
+    kaomoji_mood = input("What mood does this kaomoji represent?:")
+    kaomoji_name = input("Give this kaomoji a name:")
+    database.database[new_kaomoji] = (kaomoji_name, kaomoji_mood)
+    print("Successfully added kaomoji!")
 
 def print_json():
     print(database.database)
 
 def write_json():
-    test_dictionary = {"smiley": ":)"}
-    json_object = json.dumps(test_dictionary, indent=4)
+    new_database = database.database
+    json_object = json.dumps(new_database, indent=4)
     with open("data/database.json", "w", encoding="utf-8") as j:
         j.write(json_object)
 
--- scripts/main.py
+++ scripts/main.py
@@ -11,12 +11,14 @@ def init():
         os.mkdir("data")
     if os.path.isfile("data/database.json"):
         print("Database found!")
+        load_database()
     else:
-        response = input("Database not found. Create new database? (Y/N)")
+        response = input("Database not found. Create new database? (Y/N): ")
         match response:
             case "Y":
                 print(":)")
                 overwrite_json()
+                load_database()
             case "N":
                 print(":(")
                 exit()
@@ -28,7 +30,30 @@ def init():
 def main():
     print("Welcome to KaomojiDB. This is just a placeholder message for now.")
     init()
-    load_database()
-    print_json()
+    while True:
+        main_menu_choice = input("What would you like to do? Please input a number to select an option.\n1. View current database\n2. Add new kaomoji to database\n3. Reset database\n4. Exit program\n")
+        match main_menu_choice:
+            case "1":
+                print("This is the current database:")
+                #TODO Make sure this is pretty-printed in the future. Can't have giant databases in a single line
+                print_json()
+            case "2":
+                add_item()
+            case "3":
+                response = input("Are you sure you want to reset database? This will delete all kaomoji stored within! (Y/N): ")
+                match response:
+                    case "Y":
+                        overwrite_json()
+                        print("Database reset. Goodbye.")
+                        exit()
+                    case "N":
+                        print("Database has been kept as is.")
+                    case _:
+                        raise Exception("Invalid response...")
+            case "4":
+                print("Goodbye!")
+                exit()
+            case _:
+                raise Exception("Invalid main menu choice")
 
 main()
